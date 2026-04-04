efdfc88cab02232b5020696c939ea0452b7ee84b
Nobody <156914335+Nobodyasidentity@users.noreply.github.com>
2026-04-04 23:45:35 +0200

    Fixed the link formatting order

--- html/md.js
+++ html/md.js
@@ -80,8 +80,8 @@ function md(text){
     text=Regex4MDLink(text,"(^|<br>)(-){3,}($|<br>)","<hr>$3");
     
     text=text.replace(/(?<!\\)(?:(?<=(?<!\\)(?:\\\\)+)|(?<!\\))!\[([^\]\x01]*)\]\(([^)]+)\)/gs,(match,alt,url)=>{return `<img src="${sanitizeURL4MD(url)}" alt="${textarea2HTML(alt)}">`;});
-    text=text.replace(/(?<!\\)(?:(?<=(?<!\\)(?:\\\\)+)|(?<!\\))\[([^\]\`]+)\]\(([^)]+)\)/gs,(match,linkText,url)=>{return `<a target="_blank" href="${sanitizeURL4MD(url)}">${linkText}</a>`;});
     text=text.replace(/(?<!\\)(?:(?<=(?<!\\)(?:\\\\)+)|(?<!\\))\+\[([^\]\x01]*)\]\(([^)]+)\)/gs,(match,linkText,url)=>{return `<a target="_blank" href="${sanitizeURL4MD(url)}" download>${linkText}</a>`;});
+    text=text.replace(/(?<!\\)(?:(?<=(?<!\\)(?:\\\\)+)|(?<!\\))\[([^\]\`]+)\]\(([^)]+)\)/gs,(match,linkText,url)=>{return `<a target="_blank" href="${sanitizeURL4MD(url)}">${linkText}</a>`;});
     
     text=Regex4MD(text,"&lt;center&gt;","&lt;/center&gt;","<center>$1</center>");
     text=Regex4MD(text,"&lt;sub&gt;","&lt;/sub&gt;","<sub>$1</sub>");
